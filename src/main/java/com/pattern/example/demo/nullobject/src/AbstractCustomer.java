af049197ce2d8d8ee919429cd8f3380ae8258d43
Eduardo A. Di Nizo <eduardodinizo@hotmail.com>
2018-09-09 02:05:55 -0300

    implementado null object

---
+++ src/main/java/com/pattern/example/demo/nullobject/src/AbstractCustomer.java
@@ -0,0 +1,9 @@
+package com.pattern.example.demo.nullobject.src;
+
+public abstract class AbstractCustomer {
+    protected String name;
+
+    public abstract boolean isNil();
+
+    public abstract String getName();
+}
